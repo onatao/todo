315c35a660862c563dbbe3c57f3229e93b890df6
Nathan <nathanbarrosdev@gmail.com>
2023-10-17 09:53:56 -0300

    release: base files

---
+++ src/test/java/com/neidev/todo/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neidev.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
